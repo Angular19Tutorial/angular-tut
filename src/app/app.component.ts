9710d9d74b6de850345b99c560657f87752760ac
surbhi <surbhibisarwal79@gmail.com>
2025-04-02 22:21:32 +0530

    custom Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent, ProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-tutorial';
-  name = 'Surbhi'
-  x=5;
-  y=7;
+  name = 'Surbhi';
+  x = 5;
+  y = 7;
 }
